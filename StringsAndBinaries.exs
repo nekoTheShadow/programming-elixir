e56341b1cdd2dfb76f7a5e7af26197873ad9da17
hajime nakamura <h.nakamura0903@gmail.com>
2020-12-28 18:52:37 +0900

    練習問題: StringsAndBinaries-2

--- StringsAndBinaries.exs
+++ StringsAndBinaries.exs
@@ -2,4 +2,8 @@ defmodule StringsAndBinaries do
   def is_printable_ascii(chars) do
     Enum.all?(chars, &(?\s <= &1 && &1 <= ?~))
   end
+
+  def anagram?(word1, word2) do
+    Enum.sort(String.to_charlist(word1)) === Enum.sort(String.to_charlist(word2))
+  end
 end
