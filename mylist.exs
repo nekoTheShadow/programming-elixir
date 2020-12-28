cf1f6af5536c249d306c563ec2c2786bb118c53e
hajime nakamura <h.nakamura0903@gmail.com>
2020-12-28 13:09:15 +0900

    練習問題: ListsAndRecursion-2

--- mylist.exs
+++ mylist.exs
@@ -6,4 +6,20 @@ defmodule MyList do
   def mapsum([head | tail], f) do
     f.(head) + mapsum(tail, f)
   end
+
+  def max([a]) do
+    a
+  end
+
+  def max([a, b]) when a <= b do
+    b
+  end
+
+  def max([a, b]) when a > b do
+    a
+  end
+
+  def max([head | tail]) do
+    max(head, max(tail))
+  end
 end
