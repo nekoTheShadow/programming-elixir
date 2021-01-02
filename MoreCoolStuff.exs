31060a52560b22607bd1e0120836f4c286dc805e
hajime nakamura <h.nakamura0903@gmail.com>
2021-01-02 14:36:02 +0900

    練習問題: MoreCoolStuff-2

--- MoreCoolStuff.exs
+++ MoreCoolStuff.exs
@@ -1,9 +1,20 @@
 defmodule CSVSigil do
   def sigil_v(csv, _opts) do
-    csv
+    rows = csv
     |> String.trim_trailing()
     |> String.split("\n")
     |> Enum.map(&String.split(&1, ","))
+
+    for row <- rows do
+      Enum.map(row, &parse/1)
+    end
+  end
+
+  defp parse(word) do
+    case Float.parse(word) do
+      {float, ""} -> float
+      _ -> word
+    end
   end
 end
 
@@ -13,11 +24,12 @@ defmodule TestCSVSigil do
   use ExUnit.Case
   import CSVSigil
 
-  test "~vシジルはcsvを2次元配列に変換する" do
+  test "~vシジルはcsvを2次元配列に変換する。セルが数値の場合はfloatに変換する" do
     actual = ~v"""
-    1,2,3
+    1,2,3.14
     cat,dog
+    1A,2B,3C
     """
-    assert actual === [["1", "2", "3"], ["cat", "dog"]]
+    assert actual === [[1.0, 2.0, 3.14], ["cat", "dog"], ["1A", "2B", "3C"]]
   end
 end
